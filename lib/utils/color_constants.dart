19fea1d3ed266b372831f10cc3877a9b50bf3587
charlie-black <boompiccasso@gmail.com>
2025-05-12 12:49:52 +0300

    initial commit

---
+++ lib/utils/color_constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+final Color kPrimaryColor = HexColor("#15645e");
+final Color kBackgroundColor = HexColor("#171717");
+final Color kContainerColor = HexColor("#272727");
+final Color kTextColor = HexColor("#ffffff");
+
+
+
---
+++ lib/utils/text_styling.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'color_constants.dart';
+
+final TextStyle kTitleStyle =  TextStyle(
+  color: kPrimaryColor,
+  fontSize: 15,
+  fontWeight: FontWeight.bold,
+);
+
+final TextStyle kNormalTextStyle =  TextStyle(
+  color: kTextColor,
+  fontSize: 12,
+  fontWeight: FontWeight.normal,
+);
+
+
